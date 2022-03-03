acc39d503c914128a40bf3b0ff17ff7c72e95191
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-03-04 00:49:08 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,15 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "./images/"
+UPLOAD_FOLDER = "./uploads/"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     
