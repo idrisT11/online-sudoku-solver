b49d4483c256de115b72aa389598837b8e015cd0
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-03-03 23:02:23 +0100

    migrated app from fastAPI to flask

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,13 @@
-from fastapi import FastAPI, Request 
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles 
-from fastapi.templating import Jinja2Templates
-import uvicorn
+from flask import Flask, request, render_template
 
-app = FastAPI()
+app = Flask(__name__)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+UPLOAD_FOLDER = "./images/"
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request:Request):
-    return templates.TemplateResponse("home.html", {"request": request})
-    
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, host="0.0.0.0",
-                reload=True, access_log=False)
-
     
+    app.run()
