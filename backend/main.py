0fe04239daddd3e90c7f0006b75331223f37ff1c
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-03-04 01:09:24 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,17 @@ def home():
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
+    image = request.files.getlist("files")
+    if image:
+        if image.read() == b"":
+            return render_template("error.html")
+
+        image_location = UPLOAD_FOLDER + image.filename
+        image.save(image_location)
+    
+    else:
+        return render_template("error.html")
+     
     return render_template('upload.html')
 
 if __name__ == "__main__":
